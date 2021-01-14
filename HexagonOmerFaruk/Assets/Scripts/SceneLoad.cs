d5ed62f94f14d0d01eb792ccad3e47be823ec599
OmerFarukSariisik <oomeroo1999@hotmail.com>
2021-01-14 17:52:27 +0300

    update

---
+++ HexagonOmerFaruk/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
